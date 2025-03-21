2d8522267d55c58162eb1915560061f70be2d641
Alberto Ferrero <albertonomo@gmail.com>
2025-03-21 20:04:49 +0100

    committing in the evening

---
+++ README.md
@@ -0,0 +1,3 @@
+# juan-keyframes-walk
+
+Playing with vue, svg and css animation
---
+++ src/components/body-part.vue
@@ -0,0 +1,22 @@
+<template>
+  <g :transform="computedTransform">
+    <slot />
+    <circle :cx="anchor.x" :cy="anchor.y" r="5" fill="blue" />
+  </g>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+  part: string
+  anchor: { x: number; y: number }
+  rotation: number
+}
+
+const props = defineProps<Props>()
+
+const computedTransform = computed(() => {
+  return `rotate(${props.rotation}, ${props.anchor.x}, ${props.anchor.y})`
+})
+</script>
